e77482f29235d6be3c620950b20ae1390af7da8a
Raaaif <raifsousa96@gmail.com>
2022-01-19 11:15:53 -0300

    Atualizado BlogPessoal

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package br.org.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import br.org.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{ //herança de JPARepository  -  Qual tabela do Banco de dados os métodos irão trabalhar? No caso tabela postagem e PK Long
 
-
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo); //Como se fosse Select * from tb_postagens Where titulo like"%titulo%;" 
 }
