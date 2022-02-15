8eba6bff2be310b96da060112621fe819451f72a
Raaaif <raifsousa96@gmail.com>
2022-02-15 00:13:53 -0300

    Adicionadas modificações react

---
+++ blog-pessoal-react/blog-pessoal/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css'
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://tm.ibxk.com.br/2021/08/26/26135558614332.jpg?ims=1120x420" alt="Imagem da tela inicial" className="img"/>
+        </>
+    );
+
+}
+
+export default Home;
